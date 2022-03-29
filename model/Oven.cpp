8fb5eaf0949bd96b621cb0d2b938e32c7e60a90e
m-przybylska <86783124+m-przybylska@users.noreply.github.com>
2022-03-29 12:36:39 +0200

    oven v.1

--- model/Oven.cpp
+++ model/Oven.cpp
@@ -9,3 +9,50 @@ Oven::Oven() {}
 Oven::~Oven() {
 
 }
+
+void Oven::putIn(int type) {
+    std::lock_guard<std::mutex> guard(notBakedMutex);
+}
+
+int Oven::takeOut(int type) {
+    int takenOut = 0;
+    std::lock_guard<std::mutex> guard(notBakedMutex);
+    for (auto it = notBaked.begin(); it != notBaked.end(); ++it){
+        if ( it->timeDoEndBacking <= 0){
+            takenOut++;
+            notBaked.erase(it);
+            --it;
+        }
+    }
+    return takenOut;
+}
+
+void Oven::bakeAll() {
+    std::lock_guard<std::mutex> guard(notBakedMutex);
+    for (pizza& pizza: notBaked){
+        pizza.bake();
+    }
+}
+
+void Oven::live(){
+    functioning = true;
+    while (functioning){
+        bakeAll();
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+}
+
+void Oven::stop() {
+    functioning = false;
+}
+
+int Oven::getRemainingTime(int i){
+    std::lock_guard<std::mutex> guard(notBakedMutex);
+    return notBaked[i].timeDoEndBacking;
+}
+
+int Oven::getSize(){
+    std::lock_guard<std::mutex> guard(notBakedMutex);
+    return notBaked.size();
+}
+
